86b74e8b1b0f9601e2888dcb37e1e3950c18b11c
FabioB8a <fabiobuitra@gmail.com>
2024-03-29 14:11:38 -0500

    Add: Has role guard

--- src/app/guards/has-role-guard.guard.ts
+++ src/app/guards/has-role-guard.guard.ts
@@ -1,5 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { StorageService } from '../services/storage.service';
+import { inject } from '@angular/core';
 
 export const hasRoleGuardGuard: CanActivateFn = (route, state) => {
-  return true;
+
+  const storageService = inject(StorageService)
+  const router = inject(Router)
+
+  const account = storageService.getSavedAccount()
+
+  if (account && account.role != route.data['role']){
+    router.navigate(['/login'])
+  }
+  
+  return true
 };
