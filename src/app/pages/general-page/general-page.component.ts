42c1bd01fcf55d0a971eafc818705841f21feeeb
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-03-29 18:12:37 -0500

    manage account component

--- src/app/pages/general-page/general-page.component.ts
+++ src/app/pages/general-page/general-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { TableAccountsComponent } from './components/table-accounts/table-accounts.component';
 import {SidebarComponent} from "../../global/sidebar/sidebar.component";
 import {GeneralInformationComponent} from "./components/general-information/general-information.component";
+import {ManageAccountComponent} from "./components/manage-account/manage-account.component";
 
 @Component({
   selector: 'app-general-page',
   standalone: true,
-  imports: [TableAccountsComponent, SidebarComponent, GeneralInformationComponent],
+  imports: [TableAccountsComponent, SidebarComponent, GeneralInformationComponent, ManageAccountComponent],
   templateUrl: './general-page.component.html',
   styleUrl: './general-page.component.css',
 })
