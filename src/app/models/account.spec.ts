d084c4c81a8dcce7ab9cb4f7cbbaf1c232bcde9b
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-03-29 15:44:50 -0500

    general information component

--- src/app/models/account.spec.ts
+++ src/app/models/account.spec.ts
@@ -2,6 +2,7 @@ import { Account } from './account';
 
 describe('Account', () => {
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new Account()).toBeTruthy();
   });
 });
--- src/app/pages/general-page/general-page.component.ts
+++ src/app/pages/general-page/general-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TableAccountsComponent } from './components/table-accounts/table-accounts.component';
 import {SidebarComponent} from "../../global/sidebar/sidebar.component";
+import {GeneralInformationComponent} from "./components/general-information/general-information.component";
 
 @Component({
   selector: 'app-general-page',
   standalone: true,
-  imports: [TableAccountsComponent, SidebarComponent],
+  imports: [TableAccountsComponent, SidebarComponent, GeneralInformationComponent],
   templateUrl: './general-page.component.html',
   styleUrl: './general-page.component.css',
 })
