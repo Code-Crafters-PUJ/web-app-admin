cb2c88d8e64472cb6299ca604e70887b3ffe53af
FabioB8a <fabiobuitra@gmail.com>
2024-03-29 19:35:42 -0500

    Final add: Config and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { hasRoleGuard } from './guards/has-role.guard';
+import { isAuthenticatedGuard } from './guards/is-authenticated.guard';
 
 export const routes: Routes = [
+
     { path: 'login', 
       component: LoginPageComponent,
-      canActivate: [hasRoleGuard],
-      data:{
-        role: 'Admin',
-      }
     }
+    /*{
+       
+        path: 'path',
+        component: PathComponent,
+        canActivate: [isAuthenticatedGuard, hasRoleGuard],
+        data:{
+        role: 'role',
+        }
+      
+    }*/
 
   ];
