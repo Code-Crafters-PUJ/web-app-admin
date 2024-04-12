265eaea34152cf34cb3d744a9eba1ed59f6250cc
parraluisa <parran_ll@javeriana.edu.co>
2024-04-11 23:09:26 -0500

    Support-page component created

---
+++ src/app/pages/support/support-page.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {NgIf} from "@angular/common";
+import {SidebarComponent} from "../../global/sidebar/sidebar.component";
+import {ActivatedRoute, Router} from "@angular/router";
+
+@Component({
+  selector: 'app-support',
+  standalone: true,
+  imports: [
+    NgIf,
+    SidebarComponent
+  ],
+  templateUrl: './support-page.component.html',
+  styleUrl: './support-page.component.css'
+})
+export class SupportPageComponent  implements OnInit{
+
+    url: string = 'general';
+
+    constructor(private route: ActivatedRoute, private router:Router) {}
+
+    ngOnInit() {
+      const currentUrl = this.router.url;
+      const segments = currentUrl.split('/');
+      const segment = segments[3];
+      const lastSegment = segments[4];
+
+      switch (segment) {
+        case 'general':
+          this.url = 'general';
+          break;
+        case 'requests':
+          if (lastSegment === 'company') {
+            this.url = 'company';
+          }else {
+            this.url = 'requests';
+          }
+          break;
+        default:
+          this.url = 'general';
+      }
+    }
+
+}
