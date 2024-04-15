c128d819f4191dec85265020abf103bb329b8831
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-04-14 23:15:20 -0500

    charts marketing

--- src/app/pages/marketing-page/components/general-marketing/general-marketing.component.ts
+++ src/app/pages/marketing-page/components/general-marketing/general-marketing.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import Chart from "chart.js/auto";
 
 @Component({
   selector: 'app-general-marketing',
@@ -10,6 +11,73 @@ import {NgOptimizedImage} from "@angular/common";
   templateUrl: './general-marketing.component.html',
   styleUrl: './general-marketing.component.css'
 })
-export class GeneralMarketingComponent {
+export class GeneralMarketingComponent implements OnInit {
+
+  public doughnutChart: any;
+  public lineChart: any;
+  ngOnInit(): void {
+
+    this.CreateDoughnutChart();
+    this.createLineChart();
+  }
+
+
+  CreateDoughnutChart() {
+    this.doughnutChart = new Chart("MyChart", {
+      type: 'doughnut',
+      data: {
+        labels: [
+          'Inactivos',
+          'Activos'
+        ],
+        datasets: [{
+          label: 'Usuarios Activos',
+          data: [35, 90],
+          backgroundColor: [
+            'rgb(245,105,29)',
+            'rgb(0, 74, 173)'
+          ],
+          hoverOffset: 4
+        }]
+      },
+      options: {
+        plugins: {
+          legend: {
+            display: true,
+            labels: {
+              font: {
+                size: 14,
+                family: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif"
+              }
+            }
+          }
+        }
+      }
+    });
+  }
+
+  createLineChart() {
+    this.lineChart = new Chart("lineChart", {
+      type: 'line',
+      data: {
+        labels: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
+        datasets: [{
+          label: 'Usuarios Conectados',
+          data: [80, 60, 25, 80, 200, 100, 110, 20, 130, 140, 40, 160],
+          borderColor: 'rgb(0,74,173)',
+          tension: 0.1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+
 
 }
