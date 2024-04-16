c5f79ce634b8aec2c35ec44fc83986b82d246b39
Felipe Castiblanco <felipe-garcia@javeriana.edu.co>
2024-04-15 23:00:14 -0500

    marketing Request

--- src/app/pages/marketing-page/components/requests-marketing/requests-marketing.component.ts
+++ src/app/pages/marketing-page/components/requests-marketing/requests-marketing.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-requests-marketing',
   standalone: true,
-  imports: [],
+    imports: [
+        NgOptimizedImage
+    ],
   templateUrl: './requests-marketing.component.html',
   styleUrl: './requests-marketing.component.css'
 })
