4461cc7c3c3b98847c81bc1d48018a60352e20ca
Sanblaster <santiagoahm@gmail.com>
2024-05-04 11:14:12 -0500

    adding https support and fixing config page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseURL: "http://10.43.100.29:8081",
+  // baseURL: "http://10.43.100.29:8081",
+  baseURL: "https://api-admin.stockwage.duckdns.org",
 };
-
